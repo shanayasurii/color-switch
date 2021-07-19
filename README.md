3da6cd6d029d98fd107b9a203bef95a9d1b6aab7
shanayasurii <87651144+shanayasurii@users.noreply.github.com>
2021-07-19 17:52:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# color-switch
+# background-colour-changer
---
+++ sketch.js
@@ -0,0 +1,50 @@
+var btn_red;
+var btn_green;
+var btn_blue
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(1000, 1000);
+
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+  
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
+
+  btn_blue = createButton("BLUE");
+  btn_blue.position(400,50);
+  btn_blue.mousePressed(blue_bg);
+}
+
+function draw() {
+  background(r,g,b);
+}
+
+
+function red_bg()
+{
+  r = 255;
+  g = 0;
+  b =0;
+}
+
+function green_bg()
+{
+  r = 0;
+  g = 255;
+  b =0;
+}
+
+
+function blue_bg()
+{
+  r = 0;
+  g = 0;
+  b = 255;
+}
